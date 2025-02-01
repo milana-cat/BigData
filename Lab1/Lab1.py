4d97112e93020e8050f4c841fe6ed8de9a982437
milana-cat <103025089+milana-cat@users.noreply.github.com>
2025-02-02 01:01:50 +0300

    add def

--- Lab1/Lab1.py
+++ Lab1/Lab1.py
@@ -4,24 +4,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.ticker as mtick
 
+
+
+def main():
 # Данные для подключения
-host = "povt-cluster.tstu.tver.ru"
-port = 5432
-user = "mpi"
-password = "135a1"
-database = "Criminal"
+ host = "povt-cluster.tstu.tver.ru"
+ port = 5432
+ user = "mpi"
+ password = "135a1"
+ database = "Criminal"
 
 # Подключение к базе данных
-try:
+ try:
     engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}?client_encoding=utf8")
     print("Подключение успешно")
-except Exception as e:
+ except Exception as e:
     print("Ошибка подключения:", e)
     exit()
 
-
 # Проверка наличия таблицы и выполнения запроса
-try:
+ try:
     query = 'SELECT * FROM person JOIN get_fit_now_member ON person.id = get_fit_now_member.person_id JOIN drivers_license ON person.license_id = drivers_license.id JOIN income ON income.ssn = person.ssn  LIMIT 1000'  # Ограничиваем выборку
     df = pd.read_sql_query(query, engine)
     print("Данные успешно загружены в DataFrame")
@@ -34,11 +36,20 @@ try:
     print("Отфильтрованный DataFrame:")
     print(filtered_df.head())
 
-    # Одномерный анализ: Построение гистограмм
+    make_plot_single(filtered_df)
+     # Многомерный анализ: Построение графиков
+    make_plot_multy(filtered_df)
+    make_boxplot(filtered_df)  
+
+ except Exception as e:
+    print("Ошибка выполнения запроса:", e)
+
+# Одномерный анализ: Построение гистограмм
+def make_plot_single(data):
     for feature in ["age", "membership_status"]:  # Количественные признаки
-        if feature in filtered_df.columns:
+        if feature in data.columns:
             plt.figure(figsize=(8, 5))
-            plt.hist(filtered_df[feature].dropna(), bins=30, edgecolor="k", alpha=0.7)
+            plt.hist(data[feature].dropna(), bins=30, edgecolor="k", alpha=0.7)
             plt.title(f"Распределение признака: {feature}")
             plt.xlabel("Статус членства" if feature == "membership_status" else feature)
             plt.ylabel("Частота")
@@ -48,41 +59,36 @@ try:
         else:
             print(f"Признак '{feature}' не найден в данных.")
 
-     # Многомерный анализ: Построение графиков
-
-    # График 1: Зависимость saleprice от acreage без категорий SINGLE FAMILY
-    filtered_data_no_single_family = filtered_df[filtered_df["membership_status"] != "regular"]
+def make_plot_multy(data):
+        # График 1: Зависимость saleprice от acreage без категорий SINGLE FAMILY
+    filtered_data_no_single_family = data[data["membership_status"] != "regular"]
 
     plt.figure(figsize=(12, 6))
     sns.scatterplot(
         data=filtered_data_no_single_family,
-        x="height",
+        x="annual_income",
         y="age",
         hue="gender",
         alpha=0.7
     )
-    plt.title("Зависимость роста от возраста без членства regular")
-    plt.xlabel("Возраст")
-    plt.ylabel("Рост")
+    plt.title("Зависимость годового дохода от возраста без членства regular")
+    plt.xlabel("заработок за год")
+    plt.ylabel("возраст")
     ax = plt.gca()
     ax.yaxis.set_major_formatter(mtick.StrMethodFormatter('{x:,.0f}'))  # Форматирование оси Y
     plt.grid(True)
-    plt.legend(title="Зависимость роста от возраста")
+    plt.legend(title="Зависимость годового дохода от возраста")
     plt.show()
 
-
+def make_boxplot(data):
     
-    # График 2: Распределение saleprice по yearbuilt и soldasvacant
     plt.figure(figsize=(12, 6))
-    sns.boxplot(data=filtered_df, x="gender", y="annual_income", hue="membership_status")
-    plt.title("Распределение возраста по росту и полу")
-    plt.xlabel("Возраст")
-    plt.ylabel("Рост")
+    sns.boxplot(data=data, x="gender", y="annual_income", hue="membership_status")
+    plt.title("Распределение доходов по статусу членства и полу")
+    plt.xlabel("пол")
+    plt.ylabel("годовой доход")
     plt.xticks(rotation=45)
-    plt.legend(title="Не указан пол")
     plt.grid(True)
     plt.show()
-    
 
-except Exception as e:
-    print("Ошибка выполнения запроса:", e)
+main()
